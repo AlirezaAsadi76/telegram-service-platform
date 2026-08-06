366dcb4e2b39782a8c09b92f48fde7901048bfe6
Nima <alirezaasadi@gmail.com>
2026-08-07 00:29:56 +0430

    feat:  price service refresh currency price and premium price usecase

--- service/priceservice/refreshCurrency.go
+++ service/priceservice/refreshCurrency.go
@@ -1,1 +1,26 @@
 package priceservice
+
+import "context"
+
+func (s Service) RefreshCurrency(ctx context.Context) error {
+
+	tonUsd, tErr := s.currency.GetTonUsdPrice(ctx)
+	if tErr != nil {
+		return tErr
+	}
+	usdIrr, uErr := s.currency.GetUsdTomanPrice(ctx)
+	if uErr != nil {
+		return uErr
+	}
+	stErr := s.repository.SetTonUsdPrice(ctx, tonUsd)
+	if stErr != nil {
+		return stErr
+	}
+	suErr := s.repository.SetUsdTomanPrice(ctx, usdIrr)
+	if suErr != nil {
+		return suErr
+
+	}
+
+	return nil
+}
--- service/priceservice/refreshPricePremium.go
+++ service/priceservice/refreshPricePremium.go
@@ -1,1 +1,17 @@
 package priceservice
+
+import "context"
+
+func (s Service) RefreshPricePremium(ctx context.Context) error {
+	const Op = "priceservice.RefreshPricePremium"
+	starsPricePrv, gErr := s.telegramPrv.GetPremiumPlans(ctx)
+	if gErr != nil {
+		return gErr
+	}
+	sErr := s.repository.SetPremiumPrices(ctx, starsPricePrv)
+	if sErr != nil {
+		return sErr
+	}
+	return nil
+
+}
