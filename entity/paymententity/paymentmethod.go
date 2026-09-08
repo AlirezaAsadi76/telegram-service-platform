314148394a581b121b52649762269356e098eab7
Nima <alirezaasadi@gmail.com>
2026-09-08 15:10:06 +0430

    recover: update PaymentMethod - delete manual

--- entity/paymententity/paymentmethod.go
+++ entity/paymententity/paymentmethod.go
@@ -5,5 +5,4 @@ type PaymentMethod string
 const (
 	PaymentMethodZarinpal PaymentMethod = "ZARINPAL"
 	PaymentMethodCrypto   PaymentMethod = "CRYPTO"
-	PaymentMethodManual   PaymentMethod = "MANUAL"
 )
