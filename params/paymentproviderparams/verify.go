78f04f88b0fd0d11362a5bb8c390813ae500fb56
Nima <alirezaasadi@gmail.com>
2026-09-11 18:34:49 +0430

    recover(paymentproviderparams): improve request and response

--- params/paymentproviderparams/verify.go
+++ params/paymentproviderparams/verify.go
@@ -1,13 +1,19 @@
 package paymentproviderparams
 
-import "telegram-service-platform/entity/paymententity"
+import (
+	"telegram-service-platform/entity"
+	"telegram-service-platform/entity/paymententity"
+)
 
 type VerifyRequest struct {
 	PaymentID    uint64
 	ExternalID   string
+	Amount       entity.Amount
+	Currency     entity.Currency
 	CallbackData map[string]any
 }
 
 type VerifyResponse struct {
-	Status paymententity.PaymentStatus
+	Status      paymententity.PaymentStatus
+	ReferenceID string
 }
