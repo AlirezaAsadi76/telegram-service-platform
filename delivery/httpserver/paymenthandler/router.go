f02c7dd777368e34220e03ca516d7a850d30f9ef
Nima <alirezaasadi@gmail.com>
2026-09-13 12:09:59 +0430

    recover(paymenthandler): clean setrouts

--- delivery/httpserver/paymenthandler/router.go
+++ delivery/httpserver/paymenthandler/router.go
@@ -1,13 +1,11 @@
 package paymenthandler
 
 import (
-	"fmt"
-
 	"github.com/labstack/echo/v5"
 )
 
 func (h *Handler) SetRoutes(e *echo.Echo) {
-	fmt.Println("SetRoutes paymentHandler")
+
 	e.GET("/payments/zarinpal/callback", h.ZarinpalCallbackHandler)
 
 }
